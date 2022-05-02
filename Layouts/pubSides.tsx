0e63b1a752cba1ea07c8a127c5ccdf0c7ce69da0
junior-boop <juniorseppo3@gmail.com>
2022-05-02 13:46:39 +0200

    sdlv

---
+++ Layouts/pubSides.tsx
@@ -0,0 +1,10 @@
+import { Component, ComponentProps } from "react"
+// this is a pub side
+
+export default function PubSide(): Component {
+    return (
+        <div>
+            je fonction
+        </div>
+    )
+}
