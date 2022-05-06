1430a659a93e55a259e18eeea2cec1ce2378d27b
junior-boop <juniorseppo3@gmail.com>
2022-05-06 18:45:46 +0200

    slv

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,13 +16,13 @@ function Vide(){
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className = {Style.STYLE}>
-        <Script
+        {/* <Script
           id='Adsense-id'
           async = {true}
           strategy = "beforeInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1899219914731834"
           crossOrigin='anonymous'
-        ></Script>
+        ></Script> */}
        <Header />
        <Vide />
       <Component {...pageProps} />
---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import {Html, Head, Main, NextScript} from "next/document";
+
+export default function Document(){
+    return(
+        <Html>
+            <Head>
+                {/* google adsense */}
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    (adsbygoogle = window.adsbygoogle || []).push({
+                        google_ad_client: 1899219914731834,
+                        enable_page_level_ads: true
+                        });
+                        `,
+                        }} />
+            </Head>
+            <body>
+                <Main/>
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
